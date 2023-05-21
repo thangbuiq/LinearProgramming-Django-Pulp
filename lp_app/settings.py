17c3996510ab80ebac3164fb964daa304d85691a
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-21 23:43:07 +0700

    @thangbuiq - Updated HTML

--- lp_app/settings.py
+++ lp_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k=jb0n^cr_s!t+gcuasxwm+hoew^t#jh(zh!$yz@lke6$00xz5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # vercel_app/settings.py
-ALLOWED_HOSTS = ['127.0.0.1', '*.vercel.app', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
