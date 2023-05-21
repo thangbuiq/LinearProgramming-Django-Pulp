913ce94a67244cfa9db80011d2e86c6d7d479f59
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-21 23:49:53 +0700

    @thangbuiq - Fix bug

--- lp_app/urls.py
+++ lp_app/urls.py
@@ -20,9 +20,8 @@ app_name = 'lp'  # Add this line to specify the app_name
 
 urlpatterns = [
     # Other URL patterns
-    # path('', include(('lp.urls', 'lp'), namespace='lp')),
+    path('', include(('lp.urls', 'lp'), namespace='lp')),
     path('admin/', admin.site.urls),
-    path('', include('lp.urls')),
 ]
 
 
