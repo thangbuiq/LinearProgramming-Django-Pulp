b6060edaa03405ad25c9e3bca106aa204c075df9
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-22 12:59:34 +0700

    @thangbuiq - Fix bug CSS

--- lp_app/settings.py
+++ lp_app/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 STATIC_ROOT = BASE_DIR / 'staticfiles_build'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
