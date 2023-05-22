7ab86233bc29e66e41dc65d504467f82440cb4bd
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-22 13:55:25 +0700

    @thangbuiq - Fix bug CSS

--- lp_app/urls.py
+++ lp_app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
