6e525f20c8c60de10933f769aff5fcd1bd7f2f87
thangbuiq <21280048@student.hcmus.edu.vn>
2023-05-22 12:23:26 +0700

    @thangbuiq - Fix bug

--- lp_app/settings.py
+++ lp_app/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'lp_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
@@ -116,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
